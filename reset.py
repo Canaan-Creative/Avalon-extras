58869dcfac6414b2e3cdd2c3cf87fe91f53055e1
Xiangfu <xiangfu@openmobilefree.net>
2013-01-20 17:53:52 +0800

    update reset

--- reset.py
+++ reset.py
@@ -9,11 +9,12 @@ parser = OptionParser()
 parser.add_option("-s", "--serial", dest="serial_port", default="/dev/ttyUSB0", help="Serial port")
 (options, args) = parser.parse_args()
 
-ser = Serial(options.serial_port, 19200, 8)
+ser = Serial(options.serial_port, 115200, 8)
 
-cmd = "0100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+cmd = "ad98320801000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009999991932333333cbcccc4c64666666fdffff7f969999992f3333b3c8cccccc616666e6"
+
 print("Push reset to device:\n" + cmd)
 ser.write(cmd.decode('hex'))
 
-res=ser.read(56)
+res=ser.read(64)
 print("Result:\n" + binascii.hexlify(res))
