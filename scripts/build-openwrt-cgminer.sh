c2ca9f19a52c5d493e473cb9fadf45b18c5d7892
Xiangfu <xiangfu@openmobilefree.net>
2013-03-19 21:34:30 +0800

    build-openwrt-cgminer.sh update

--- scripts/build-openwrt-cgminer.sh
+++ scripts/build-openwrt-cgminer.sh
@@ -1,4 +1,11 @@
-IP=192.168.0.100
+#!/bin/bash
+
+PATH=/home/xiangfu/workspace/PanGu/openwrt/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin:$PATH
+STAGING_DIR=/home/xiangfu/workspace/PanGu/openwrt/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2
+export PATH
+export STAGING_DIR
+
+IP=$1
 
 cp ${HOME}/workspace/bitcoin/avalon/cgminer/driver-avalon.c \
      ${HOME}/workspace/PanGu/openwrt/build_dir/target-mips_r2_uClibc-0.9.33.2/cgminer-20130108 && \
@@ -6,10 +13,6 @@ cp ${HOME}/workspace/bitcoin/avalon/cgminer/driver-avalon.h \
      ${HOME}/workspace/PanGu/openwrt/build_dir/target-mips_r2_uClibc-0.9.33.2/cgminer-20130108 && \
 cp ${HOME}/workspace/bitcoin/avalon/cgminer/cgminer.c       \
      ${HOME}/workspace/PanGu/openwrt/build_dir/target-mips_r2_uClibc-0.9.33.2/cgminer-20130108 && \
-make -C build_dir/target-mips_r2_uClibc-0.9.33.2/cgminer-20130108 && \
-cp ./build_dir/target-mips_r2_uClibc-0.9.33.2/cgminer-20130108/cgminer ./cgminer-mips && \
-scp cgminer-mips root@$IP:/tmp/cgminer && \
-scp ./r root@$IP:/tmp/ 
-
-
-#ssh-keygen -f "${HOME}/.ssh/known_hosts" -R $IP && \
+make -C ${HOME}/workspace/PanGu/openwrt/build_dir/target-mips_r2_uClibc-0.9.33.2/cgminer-20130108 && \
+cp ${HOME}/workspace/PanGu/openwrt/build_dir/target-mips_r2_uClibc-0.9.33.2/cgminer-20130108/cgminer ./cgminer-mips && \
+scp cgminer-mips root@$IP:/tmp/
