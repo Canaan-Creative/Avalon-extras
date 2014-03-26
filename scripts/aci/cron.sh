1932d5666d097e94c5c5445bdf4c40ad82bd129e
ECL Time <ecl.time@gmail.com>
2014-03-26 10:23:06 +0800

    Add Automated Continuous Integration (aci) script and example configurations

---
+++ scripts/aci/cron.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# crontab -l
+#
+## m h  dom mon dow   command
+#00 * * * * ~/aci/cron.sh
+#30 * * * * ~/aci/cron.sh
+
+export PATH=~/bin:$PATH
+BASEDIR=$(readlink -e $(dirname $0))
+
+. $BASEDIR/aci.conf
+FLAG=$WORKDIR/.check
+
+cd $WORKDIR
+i=0
+while [ -e $FLAG ]; do
+        [ "$i" == "0" ] && echo "Wait for other checking process ..."
+        sleep 10
+        i=`expr $i + 1`
+        # 25min
+        [ "$i" -ge "`expr $TIMEOUT \* 6`" ] && echo "[ERROR]: Timed out" && my_mail "Avalon Check Timed Out" "`date`" && exit
+done
+touch $FLAG
+
+[ ! -d $WORKDIR/log ] && mkdir -p $WORKDIR/log
+LOG=$WORKDIR/log/`date +%Y%m%d_%H%M`.log
+
+# Automated Continuous Integration
+$BASEDIR/aci.sh > $LOG 2>&1
+RET="$?"
+[ "$RET" != "0" ] && my_mail "Build Error $RET" "tail -256 $LOG"
+[ "$RET" == "0" ] && grep "Revision has not been changed" $LOG > /dev/null 2>&1 && rm $LOG
+
+rm -f $FLAG
+
