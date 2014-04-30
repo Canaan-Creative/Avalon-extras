6b318faaddf98450f040a88cd083ed436e78b84c
Mikeqin <Fengling.Qin@gmail.com>
2014-04-30 17:14:12 +0800

    Add nonce value for comparison

--- scripts/a3233_avalon_usb_test.py
+++ scripts/a3233_avalon_usb_test.py
@@ -7,6 +7,7 @@ import binascii
 parser = OptionParser()
 parser.add_option("-s", "--serial", dest="serial_port", default="/dev/ttyACM0", help="Serial port")
 parser.add_option("-d", "--data", dest="data", default="178ab19c1e0dc9651d37418fbbf44b976dfd4571c09241c49564141267eff8d8000000000000000000000000000000000000000001d0c14a507051881a057e08", help="Testing data")
+parser.add_option("-n", "--nonce", dest="nonce", default="null", help="nonce")
 
 (options, args) = parser.parse_args()
 
@@ -19,3 +20,4 @@ ser.write(payload.decode('hex'))
 
 res=ser.read(4)
 print("Result: " + binascii.hexlify(res))
+print("Nonce:  " + options.nonce)
