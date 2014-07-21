2f63b27adc1f1c5964aa3e45c281157f4a8c3899
Mikeqin <Fengling.Qin@gmail.com>
2014-07-21 16:05:12 +0800

    Fix mismatch asci_cnt

--- scripts/avalon-mm-modular-test.py
+++ scripts/avalon-mm-modular-test.py
@@ -67,7 +67,7 @@ def run_test(cmd):
 			print(str(count) + ": Something is wrong or modular id not correct")
 		else :
 			result = binascii.hexlify(res_s)
-			for i in range(0, asic_cnt):
+			for i in range(0, asic_cnt+1):
 				number = '{:03}'.format(int(result[10 + i * 2:12 + i * 2], 16))
 				if (i == 0):
 					sys.stdout.write(number + ":\t")
