efbdc3257cc2b2991e2575e159885b6f704e7952
Mikeqin <Fengling.Qin@gmail.com>
2014-08-06 11:38:23 +0800

    Add avalon3.5 modular test

--- scripts/avalon3.5-mm-modular-test.py
+++ scripts/avalon3.5-mm-modular-test.py
@@ -29,6 +29,15 @@ import sys
 import usb.core
 import usb.util
 import binascii
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-m", "--module", dest="module_id", default="0", help="Module ID: 0 - 3, default:0")
+parser.add_option("-M", "--Mode", dest="run_mode", default="0", help="Run Mode:0-Normal,1-Loopback test; default:0")
+(options, args) = parser.parse_args()
+
+asic_cnt = 10
+miner_cnt = 5
 
 def enum_usbhid(vendor_id, product_id):
     # Find device
@@ -70,30 +79,32 @@ def hid_req(hiddev, endpin, endpout, addr, req, data):
                 "0000" +    \
                 req +   \
                 data
-    else:
+        hiddev.write(endpout, txdat.decode("hex"))
+
+    if req == '03' or req == '05':
         data = data.ljust(112, '0')
         datalen = 8 + len(data)
         txdat = hex(datalen)[2:].rjust(2, '0') +    \
                 "0000" +    \
-                req + \
+                "05" + \
                 "280000" +  \
                 addr.rjust(2, '0') +    \
                 data
         hiddev.write(endpout, txdat.decode("hex"))
-        hiddev.read(endpin, 64, 100)
+        hiddev.read(endpin, 64)
 
+    if req == '04' or req == '05':
         datalen = 8
         txdat = hex(datalen)[2:].rjust(2, '0') +    \
                 "0000" +    \
-                req + \
+                "05" + \
                 "002800" +  \
                 addr.rjust(2, '0') +    \
                 "0".ljust(112, '0')
-
-    hiddev.write(endpout, txdat.decode("hex"))
+        hiddev.write(endpout, txdat.decode("hex"))
 
 def hid_read(hiddev, endpin):
-    ret = hiddev.read(endpin, 64, 100)
+    ret = hiddev.read(endpin, 64)
     if ret[0] > 4:
         return ret[4:ret[0]]
     else:
@@ -113,7 +124,7 @@ def run_loopback():
     if ret:
         print "Device version: " +  ''.join([chr(x) for x in ret])
     else:
-        print "Devcie Ver null"
+        print "Devcie version null"
 
     # addressing 0x18
     ret = hid_xfer(hiddev, endpin, endpout, "00", "05", "0000000000000018")
@@ -130,7 +141,7 @@ def run_loopback():
         if ret:
             rxdat = binascii.hexlify(ret)
             if rxdat == (txdat +  str(i).rjust(2,'0')):
-                print "Loopback success " + str(i)
+                print "Loopback success" + str(i)
             else:
                 print "txdat = " + txdat
                 print "rxdat = " + rxdat
@@ -139,6 +150,119 @@ def run_loopback():
         else:
             print "Read None"
 
+TYPE_TEST = "14"
+TYPE_DETECT = "0a"
+TYPE_REQUIRE = "12"
+
+def CRC16(message):
+	#CRC-16-CITT poly, the CRC sheme used by ymodem protocol
+	poly = 0x1021
+	 #16bit operation register, initialized to zeros
+	reg = 0x0000
+	#pad the end of the message with the size of the poly
+	message += '\x00\x00'
+	#for each bit in the message
+	for byte in message:
+		mask = 0x80
+		while(mask > 0):
+			#left shift by one
+			reg<<=1
+			#input the next bit from the message into the right hand side of the op reg
+			if ord(byte) & mask:
+				reg += 1
+			mask>>=1
+			#if a one popped out the left of the reg, xor reg w/poly
+			if reg > 0xffff:
+				#eliminate any one that popped out the left
+				reg &= 0xffff
+				#xor with the poly, this is the remainder
+				reg ^= poly
+	return reg
+
+def mm_package(cmd_type, module_id):
+	data = "000000000000000000000000000000000000000000000000000000000000" + module_id.rjust(4, '0')
+	crc = CRC16(data.decode("hex"))
+	return "4156" + cmd_type + "0101" + data + hex(crc)[2:].rjust(4, '0')
+
+def run_test(hiddev, endpin, endpout, cmd):
+        hid_req(hiddev, endpin, endpout, "00", "03", cmd)
+	for count in range(0, miner_cnt):
+                while True:
+                    hid_req(hiddev, endpin, endpout, "00", "04", cmd)
+                    res_s = hid_read(hiddev, endpin)
+                    if res_s != None:
+                        break
+
+		if not res_s:
+			print(str(count) + ": Something is wrong or modular id not correct")
+		else :
+			result = binascii.hexlify(res_s)
+			for i in range(0, asic_cnt+1):
+				number = '{:03}'.format(int(result[10 + i * 2:12 + i * 2], 16))
+				if (i == 0):
+					sys.stdout.write(number + ":\t")
+				else :
+					sys.stdout.write(number + "\t")
+				sys.stdout.flush()
+			print("")
+
+
+def run_detect(hiddev, endpin, endpout, cmd):
+	#version
+        res_s = hid_xfer(hiddev, endpin, endpout, "00", "05", cmd)
+	if not res_s:
+		print("ver:Something is wrong or modular id not correct")
+	else :
+		print("ver:" + ''.join([chr(x) for x in res_s])[3:20])
+
+def run_require(hiddev, endpin, endpout, cmd):
+        res_s = hid_xfer(hiddev, endpin, endpout, "00", "05", cmd)
+	if not res_s:
+		print("status:Something is wrong or modular id not correct")
+	else :
+		# format: temp(40|50), fan(20|30), freq(300), vol(400), localwork(1), g_hw_work(300), pg(0)
+		avalon_require = binascii.hexlify(res_s)
+		temp1 = int(avalon_require[10:14], 16)
+		temp2 = int(avalon_require[14:18], 16)
+		fan1 = int(avalon_require[18:22], 16)
+		fan2 = int(avalon_require[22:26], 16)
+		freq = int(avalon_require[26:34], 16)
+		vol = int(avalon_require[34:42], 16)
+		localwork = int(avalon_require[42:50], 16)
+		g_hw_work = int(avalon_require[50:58], 16)
+		pg = int(avalon_require[58:66], 16)
+		result = "status:temp(" + str(temp1) + "," + str(temp2) + "), "
+		result = result + "fan1(" + str(fan1) + "," + str(fan2) + "), "
+		result = result + "freq(" + str(freq) + "), "
+		result = result + "vol(" + str(vol) + "), "
+		result = result + "localwork(" + str(localwork) + "), "
+		result = result + "g_hw_work(" + str(g_hw_work) + "), "
+		result = result + "pg(" + str(pg) + ")"
+		print(result)
+
+
+def run_modular_test():
+    hid_vid = 0x1fc9
+    hid_pid = 0x0088
+
+    hiddev, endpin, endpout = enum_usbhid(hid_vid, hid_pid)
+    ret = hid_xfer(hiddev, endpin, endpout, "00", "01", "40420f00")
+    if ret:
+        print "USB2IIC version: " +  ''.join([chr(x) for x in ret])
+    else:
+        print "USB2IIC version null"
+
+    while True:
+        print("Reading result ...")
+        print("module id:" + options.module_id)
+        run_detect(hiddev, endpin, endpout, mm_package(TYPE_DETECT, options.module_id))
+        run_require(hiddev, endpin, endpout, mm_package(TYPE_REQUIRE, options.module_id))
+        run_test(hiddev, endpin, endpout, mm_package(TYPE_TEST, options.module_id))
+        raw_input('Press enter to continue:')
+
 if __name__ == '__main__':
-    run_loopback()
+    if options.run_mode == '0':
+        run_modular_test()
+    else:
+        run_loopback()
 
