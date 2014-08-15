9042b9147d4c680318d4e74a4048c6be89af8b94
archang <archang@0xf8.com>
2014-08-15 18:45:00 +0800

    Minor fixes.

--- farm-manager/utils/rtac/rtacThread.py
+++ farm-manager/utils/rtac/rtacThread.py
@@ -10,48 +10,47 @@ def sshThread(hostQueue, lock, commands, passwd, retry):
     while True:
         try:
             (hostIP, hostID) = hostQueue.get(False)
-            for i in range(0, retry):
-                ssh = paramiko.SSHClient()
-                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                errorFlag = False
-                for k in range(0, retry):
-                    try:
-                        ssh.connect(hostIP, 22, 'root', passwd)
-                        break
-                    except:
-                        ssh.close()
-                        if k < retry - 1:
-                            lock.acquire()
-                            print('\033[1m\033[33mCannot connect to ' +
-                                  hostIP + '. Try Again.\033[0m')
-                            lock.release()
-                        else:
-                            lock.acquire()
-                            print('\033[31mCannot connect to ' + hostIP +
-                                  '. Skip.\033[0m')
-                            lock.release()
-                            errorFlag = True
-                if errorFlag:
-                    break
+        except:
+            break
+        for i in range(0, retry):
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            errorFlag = False
+            for k in range(0, retry):
                 try:
-                    for c in commands:
-                        stdin, stdout, stderr = ssh.exec_command(c)
+                    ssh.connect(hostIP, 22, 'root', passwd)
+                    break
                 except:
                     ssh.close()
-                    lock.acquire()
-                    print("\033[31mConnection to " + hostIP +
-                          " lost. Extend time-out and try again.\033[0m")
-                    lock.release()
-                    continue
-
-                ssh.close()
-
-                lock.acquire()
-                print("Update complete @" + hostIP + ".")
-                lock.release()
+                    if k < retry - 1:
+                        lock.acquire()
+                        print('\033[1m\033[33mCannot connect to ' +
+                              hostIP + '. Try Again.\033[0m')
+                        lock.release()
+                    else:
+                        lock.acquire()
+                        print('\033[31mCannot connect to ' + hostIP +
+                              '. Skip.\033[0m')
+                        lock.release()
+                        errorFlag = True
+            if errorFlag:
                 break
+            try:
+                for c in commands:
+                    stdin, stdout, stderr = ssh.exec_command(c)
+            except:
+                ssh.close()
+                lock.acquire()
+                print("\033[31mConnection to " + hostIP +
+                      " lost. Extend time-out and try again.\033[0m")
+                lock.release()
+                continue
 
-        except:
+            ssh.close()
+
+            lock.acquire()
+            print("Update complete @" + hostIP + ".")
+            lock.release()
             break
 
 
@@ -60,55 +59,55 @@ def telnetThread(hostQueue, lock, commands, flag, retry):
     while True:
         try:
             (hostIP, hostID) = hostQueue.get(False)
-            for i in range(0, retry):
-                tn = telnetlib.Telnet()
+        except:
+            break
+        for i in range(0, retry):
+            tn = telnetlib.Telnet()
 
-                errorFlag = False
-                for k in range(0, retry):
-                    # try connecting for some times
-                    try:
-                        tn.open(hostIP, 23)
-                        break
-                    except:
-                        tn.close()
-                        if k < retry - 1:
-                            lock.acquire()
-                            print('\033[1m\033[33mCannot connect to ' +
-                                  hostIP + '. Try Again.\033[0m')
-                            lock.release()
-                        else:
-                            lock.acquire()
-                            print('\033[31mCannot connect to ' + hostIP +
-                                  '. Skip.\033[0m')
-                            lock.release()
-                            errorFlag = True
-                if errorFlag:
-                    break
-
+            errorFlag = False
+            for k in range(0, retry):
+                # try connecting for some times
                 try:
-                    tn.read_until(flag)
-                    for command in commands:
-                        if isinstance(command, basestring):
-                            tn.write(command + '\n')
-                            tn.read_until(flag)
-                        else:
-                            tn.write(command[0] + '\n')
-                            tn.read_until(command[1])
-                    tn.write('exit\n')
-                    tn.read_all()
+                    tn.open(hostIP, 23)
+                    break
                 except:
                     tn.close()
-                    lock.acquire()
-                    print("\033[31mConnection to " + hostIP +
-                          " lost. Extend time-out and try again.\033[0m")
-                    lock.release()
-                    continue
-
-                tn.close()
-
-                lock.acquire()
-                print("Update complete @" + hostIP + ".")
-                lock.release()
+                    if k < retry - 1:
+                        lock.acquire()
+                        print('\033[1m\033[33mCannot connect to ' +
+                              hostIP + '. Try Again.\033[0m')
+                        lock.release()
+                    else:
+                        lock.acquire()
+                        print('\033[31mCannot connect to ' + hostIP +
+                              '. Skip.\033[0m')
+                        lock.release()
+                        errorFlag = True
+            if errorFlag:
                 break
-        except:
+
+            try:
+                tn.read_until(flag)
+                for command in commands:
+                    if isinstance(command, basestring):
+                        tn.write(command + '\n')
+                        tn.read_until(flag)
+                    else:
+                        tn.write(command[0] + '\n')
+                        tn.read_until(command[1])
+                tn.write('exit\n')
+                tn.read_all()
+            except:
+                tn.close()
+                lock.acquire()
+                print("\033[31mConnection to " + hostIP +
+                      " lost. Extend time-out and try again.\033[0m")
+                lock.release()
+                continue
+
+            tn.close()
+
+            lock.acquire()
+            print("Update complete @" + hostIP + ".")
+            lock.release()
             break
