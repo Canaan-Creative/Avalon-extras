d6d4b6a89278ad00c2a9a92dcff89e74b5cc94ce
Mikeqin <Fengling.Qin@gmail.com>
2014-12-01 17:27:18 +0800

    Ping an invalid address when used as delay

--- scripts/avalon4/auc_programmer/auc_clibatch.bat
+++ scripts/avalon4/auc_programmer/auc_clibatch.bat
@@ -1,12 +1,12 @@
 @echo off
 :startprog
-echo %date%_%time%   "烧写开始"
+echo %date%_%time%   烧写开始
 C:\Keil_v5\UV4\UV4 -f auc_programmer.uvproj -j0 -O avalon-usb-converter.log
 if %ERRORLEVEL% EQU 0 (
-	echo "烧写完成，请更换下一个"
+	echo 烧写完成，请更换下一个
 ) else (
-	echo "烧写未成功，请检查！"
+	echo 烧写未成功，请检查！
 )
-REM sleep for 2 seconds
-ping -N 2 127.0.0.1 > NUL
+::sleep for 2 seconds
+ping 10.11.12.13 -n 2 -w 1000 > nul
 goto :startprog
