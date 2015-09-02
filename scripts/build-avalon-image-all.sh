b86f7c873718fd76b0ceb2f165749acdba3b63ba
Mikeqin <Fengling.Qin@gmail.com>
2015-09-02 09:00:42 +0800

    Compile Raspberry Pi firmware first

--- scripts/build-avalon-image-all.sh
+++ scripts/build-avalon-image-all.sh
@@ -13,17 +13,17 @@ $DL_PROG https://github.com/Canaan-Creative/avalon-extras/raw/master/scripts/bui
 chmod 0755 build-avalon-image.sh
 mkdir -p avalon/bin
 
-MACHINE=$MACHINE AVA_TARGET_BOARD=tl-wr703n-v1 ./build-avalon-image.sh --build        && \
-MACHINE=$MACHINE AVA_TARGET_BOARD=tl-wr703n-v1 ./build-avalon-image.sh                && \
-MACHINE=$MACHINE AVA_TARGET_BOARD=tl-wr703n-v1 ./build-avalon-image.sh --cgminer      && \
-echo "==========================================================="   && \
-echo "==================== tl-wr703n-v1 DONE ===================="   && \
-echo "==========================================================="   && \
 MACHINE=$MACHINE AVA_TARGET_BOARD=pi-modelb-v2 ./build-avalon-image.sh --build        && \
 MACHINE=$MACHINE AVA_TARGET_BOARD=pi-modelb-v2 ./build-avalon-image.sh                && \
 MACHINE=$MACHINE AVA_TARGET_BOARD=pi-modelb-v2 ./build-avalon-image.sh --cgminer      && \
 echo "==========================================================="   && \
 echo "==================== pi-modelb-v2 DONE ===================="   && \
+echo "==========================================================="   && \
+MACHINE=$MACHINE AVA_TARGET_BOARD=tl-wr703n-v1 ./build-avalon-image.sh --build        && \
+MACHINE=$MACHINE AVA_TARGET_BOARD=tl-wr703n-v1 ./build-avalon-image.sh                && \
+MACHINE=$MACHINE AVA_TARGET_BOARD=tl-wr703n-v1 ./build-avalon-image.sh --cgminer      && \
+echo "==========================================================="   && \
+echo "==================== tl-wr703n-v1 DONE ===================="   && \
 echo "==========================================================="
 
 rm -rf avalon/[cdlo]* build-avalon-image.sh
