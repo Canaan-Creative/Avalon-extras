4d97c4a8325b2e60e625cfc433e2591e9939bef4
Mikeqin <Fengling.Qin@gmail.com>
2015-12-24 12:43:03 +0800

    Update build-avalon-image.sh

--- scripts/build-avalon-image.sh
+++ scripts/build-avalon-image.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
+# This is a script for build avalon controller image
+# ROOT_DIR is avalon
+# OPENWRT_DIR is ${ROOT_DIR}/openwrt, build the image in it
+#
+# Controller's image should include the following configurations:
+# ${AVA_MACHINE}_owrepo : OpenWrt repo, format: repo_url@repo_ver
+# feeds.${AVA_MACHINE}.conf : OpenWrt feeds, file locate in cgminer-openwrt-packages
+# ${AVA_TARGET_BOARD}_brdcfg : OpenWrt target and config, file locate in cgminer-openwrt-packages
+# Learn bash: http://explainshell.com/
+set -e
 
-#AVA_MACHINE=avalon1
-#AVA_MACHINE=avalon2                         # Support Avalon2/MM firmware
-#AVA_MACHINE=avalon4                          # Support Avalon4/MM firmware
-AVA_MACHINE=avalon6                          # Support Avalon6/MM firmware
-[ -z "${AVA_MACHINE}" ] && AVA_MACHINE=avalon4
+SCRIPT_VERSION=20151223
 
-#AVA_TARGET_BOARD=tl-wr703n-v1   # TP-Link WR703N-v1
-#AVA_TARGET_BOARD=pi-modelb-v2   # Raspberry-Pi ModelB-v2
-[ -z "${AVA_TARGET_BOARD}" ]  && AVA_TARGET_BOARD=tl-wr703n-v1  # TP-Link WR703N-v1
+# Support machine: avalon6, avalon4
+[ -z "${AVA_MACHINE}" ] && AVA_MACHINE=avalon6
 
-OPENWRT_CONFIG=""
-[ "${AVA_TARGET_BOARD}" == "tl-wr703n-v1" ] && AVA_TARGET_PLATFORM=ar71xx && OPENWRT_CONFIG=config.${AVA_MACHINE}.703n
-[ "${AVA_TARGET_BOARD}" == "pi-modelb-v2" ] && AVA_TARGET_PLATFORM=brcm2708 && OPENWRT_CONFIG=config.${AVA_MACHINE}.raspberry-pi
-[ -z "${OPENWRT_CONFIG}" ] && echo "[ERROR]: Target board not suported" && exit 1
+# Support target board: pi-modelb-v2, pi-modelb-v1, tl-wr703n-v1, tl-mr3020-v1
+[ -z "${AVA_TARGET_BOARD}" ] && AVA_TARGET_BOARD=pi-modelb-v1
+
+# OpenWrt repo
+avalon4_owrepo="svn://svn.openwrt.org/openwrt/trunk@43076"
+avalon6_owrepo="svn://svn.openwrt.org/openwrt/trunk@43076"
+
+# OpenWrt feeds
+FEEDS_CONF=feeds.${AVA_MACHINE}.conf
+
+# Board config: target(get it in the OpenWrt bin), config
+pi_modelb_v2_brdcfg=("brcm2709" "config.${AVA_MACHINE}.rpi2")
+pi_modelb_v1_brdcfg=("brcm2708" "config.${AVA_MACHINE}.raspberry-pi")
+tl_wr703n_v1_brdcfg=("ar71xx" "config.${AVA_MACHINE}.703n")
+tl_mr3020_v1_brdcfg=("ar71xx" "config.${AVA_MACHINE}.703n")
 
 which wget > /dev/null && DL_PROG=wget && DL_PARA="-nv -O"
 which curl > /dev/null && DL_PROG=curl && DL_PARA="-L -o"
-
-VERSION=20140415
-OPENWRT_PATH=./openwrt
-OPENWRT_VER=41240
-[ "${AVA_MACHINE}" == "avalon2" ] && OPENWRT_VER=41240
-[ "${AVA_MACHINE}" == "avalon4" ] && OPENWRT_VER=43076
-[ "${AVA_MACHINE}" == "avalon6" ] && OPENWRT_VER=43076
-LUCI_PATH=./luci
 
 # According to http://wiki.openwrt.org/doc/howto/build
 unset SED
@@ -32,187 +40,151 @@ unset GREP_OPTIONS
 [ "`id -u`" == "0" ] && echo "[ERROR]: Please use non-root user" && exit 1
 CORE_NUM="$(expr $(nproc) + 1)"
 [ -z "$CORE_NUM" ] && CORE_NUM=2
+DATE=`date +%Y%m%d`
+SCRIPT_FILE="$(readlink -f $0)"
+SCRIPT_DIR=`dirname ${SCRIPT_FILE}`
+ROOT_DIR=${SCRIPT_DIR}/avalon
+OPENWRT_DIR=${ROOT_DIR}/openwrt
 
-## Version
-if [ "$#" == "0" -a ! -d ./avalon ]; then
-    $0 --help
-    exit 0
-elif [ "$1" == "--version" ] || [ "$1" == "--help" ]; then
+prepare_version() {
+    cd ${OPENWRT_DIR}
+    GIT_VERSION=`git ls-remote https://github.com/Canaan-Creative/cgminer avalon4 | cut -f1 | cut -c1-7`
+    LUCI_GIT_VERSION=`git --git-dir=./feeds/luci/.git rev-parse HEAD | cut -c1-7`
+    OW_GIT_VERSION=`git --git-dir=./feeds/cgminer/.git rev-parse HEAD | cut -c1-7`
+
+    cat > ./files/etc/avalon_version << EOL
+Avalon Firmware - $DATE
+    luci: $LUCI_GIT_VERSION
+    cgminer: $GIT_VERSION
+    cgminer-packages: $OW_GIT_VERSION
+EOL
+}
+
+prepare_config() {
+    cd ${OPENWRT_DIR}
+    eval OPENWRT_CONFIG=\${"`echo ${AVA_TARGET_BOARD//-/_}`"_brdcfg[1]} && cp ./feeds/cgminer/cgminer/data/${OPENWRT_CONFIG} .config
+}
+
+prepare_feeds() {
+    cd ${OPENWRT_DIR}
+    $DL_PROG https://raw.github.com/Canaan-Creative/cgminer-openwrt-packages/master/cgminer/data/${FEEDS_CONF} $DL_PARA feeds.conf && \
+    ./scripts/feeds update -a && \
+    ./scripts/feeds install -a
+
+    if [ ! -e files ]; then
+        ln -s feeds/cgminer/cgminer/root-files files
+    fi
+}
+
+prepare_source() {
+    echo "Gen firmware for ${AVA_TARGET_BOARD}:${AVA_MACHINE}"
+    cd ${SCRIPT_DIR}
+    [ ! -d avalon ] && mkdir -p avalon/bin
+    cd avalon
+    if [ ! -d openwrt ]; then
+        eval OPENWRT_URL=\${${AVA_MACHINE}_owrepo}
+        PROTOCOL="`echo ${OPENWRT_URL} | cut -d : -f 1`"
+
+        case "${PROTOCOL}" in
+            git)
+                GITBRANCH="`echo ${OPENWRT_URL} | cut -d @ -f 2`"
+                GITREPO="`echo ${OPENWRT_URL} | cut -d @ -f 1`"
+                git clone -b ${GITBRANCH} ${GITREPO} openwrt
+                ;;
+            svn)
+                SVNVER="`echo ${OPENWRT_URL} | cut -d @ -f 2`"
+                SVNREPO="`echo ${OPENWRT_URL} | cut -d @ -f 1`"
+                svn co ${SVNREPO}@${SVNVER} openwrt
+                ;;
+            *)
+                echo "Protocol not supported"; exit 1;
+                ;;
+        esac
+    fi
+}
+
+build_image() {
+    cd ${OPENWRT_DIR}
+    yes "" | make oldconfig > /dev/null
+    make -j${CORE_NUM}
+}
+
+build_cgminer() {
+    cd ${OPENWRT_DIR}
+    rm -f ./dl/cgminer-*-avalon*.tar.bz2
+    yes "" | make oldconfig > /dev/null
+    make -j${CORE_NUM} package/cgminer/{clean,compile}
+    if [ "$?" == "0" ]; then
+        eval AVA_TARGET_PLATFORM=\${"`echo ${AVA_TARGET_BOARD//-/_}`"_brdcfg[0]}
+        cd ..
+        mkdir -p ./bin/${AVA_TARGET_BOARD}
+        cp ./openwrt/bin/${AVA_TARGET_PLATFORM}/packages/cgminer/cgminer*.ipk  ./bin/${AVA_TARGET_BOARD}
+    fi
+}
+
+do_release() {
+    cd ${ROOT_DIR}
+    eval AVA_TARGET_PLATFORM=\${"`echo ${AVA_TARGET_BOARD//-/_}`"_brdcfg[0]}
+    mkdir -p ./bin/${DATE}/${AVA_TARGET_BOARD}/
+    cp -a ./openwrt/bin/${AVA_TARGET_PLATFORM}/* ./bin/${DATE}/${AVA_TARGET_BOARD}/
+}
+
+cleanup() {
+    cd ${ROOT_DIR}
+    rm -rf openwrt/ > /dev/null
+}
+
+show_help() {
     echo "\
-
-Usage: $0 [--version] [--help] [--clone] [--build] [--update] [--cgminer]
+Usage: $0 [--version] [--help] [--build] [--cgminer] [--cleanup]
 
      --version
      --help             Display help message
 
-     --clone            Get all source code, ONLY NEED ONCE
-
      --build            Get .config file and build firmware
-
-     --update           Update all repos
 
      --cgminer          Re-compile only cgminer openwrt package
 
-Without any parameter I will build the Avalon firmware. make sure you run
-  [$0 --clone] ONCE for get all sources
-
-     --removeall        Remove all files
+     --cleanup          Remove all files
 
      AVA_TARGET_BOARD   Environment variable, available target:
-                        tl-wr703n-v1, pi-modelb-v2
-                        use tl-wr703n-v1 if unset
+                        tl-wr703n-v1, pi-modelb-v1
+                        pi-modelb-v2, tl-mr3020-v1
+                        use pi-modelb-v2 if unset
+
+     AVA_MACHINE        Environment variable, available machine:
+                        avalon6, avalon4
+                        use avalon6 if unset
 
 Written by: Xiangfu <xiangfu@openmobilefree.net>
-                    19BT2rcGStUK23vwrmF6y6s3ZWpxzQQn8x
+            Fengling <Fengling.Qin@gmail.com>
+                                                     Version: ${SCRIPT_VERSION}"
+ }
 
-                                                     Version: ${VERSION}"
+if [ "$#" == "0" ]; then
+    $0 --help
     exit 0
 fi
 
-
-## Remove
-if [ "$1" == "--removeall" ]; then
-    rm -rf avalon/cgminer avalon/cgminer-openwrt-packages/ avalon/luci/ avalon/openwrt/
-    exit $?
-fi
-
-
-## Init
-if [ "$1" == "--clone" ]; then
-    [ ! -d avalon ] && mkdir -p avalon/bin
-    cd avalon
-    svn co svn://svn.openwrt.org/openwrt/trunk@${OPENWRT_VER} openwrt
-    git clone git://github.com/Canaan-Creative/cgminer.git
-    git clone git://github.com/Canaan-Creative/cgminer-openwrt-packages.git
-    git clone git://github.com/Canaan-Creative/luci.git
-
-    if [ "${AVA_MACHINE}" == "avalon1" ]; then
-        (cd luci && git checkout -b cgminer-webui origin/cgminer-webui)
-    fi
-    if [ "${AVA_MACHINE}" == "avalon2" ]; then
-        (cd cgminer && git checkout -b avalon2 origin/avalon2)
-        (cd luci && git checkout -b cgminer-webui-avalon2 origin/cgminer-webui-avalon2)
-    fi
-    if [ "${AVA_MACHINE}" == "avalon4" ]; then
-        (cd cgminer && git checkout -b avalon4 origin/avalon4)
-        (cd luci && git checkout -b cgminer-webui-avalon4 origin/cgminer-webui-avalon4)
-    fi
-    if [ "${AVA_MACHINE}" == "avalon6" ]; then
-        (cd cgminer && git checkout -b avalon4 origin/avalon4)
-        (cd luci && git checkout -b cgminer-webui-avalon6 origin/cgminer-webui-avalon6)
-    fi
-
-
-    cd openwrt
-    [ -d ../../dl ] && ln -sf ../dl ../dl
-    [ ! -e ../dl ] && mkdir ../dl
-    ln -sf ../dl
-    if [ "${AVA_MACHINE}" == "avalon4" ] || [ "${AVA_MACHINE}" == "avalon6" ]; then
-        $DL_PROG https://raw.github.com/Canaan-Creative/cgminer-openwrt-packages/master/cgminer/data/feeds.${AVA_MACHINE}.conf $DL_PARA feeds.conf
-    else
-        $DL_PROG https://raw.github.com/Canaan-Creative/cgminer-openwrt-packages/master/cgminer/data/feeds.conf $DL_PARA feeds.conf
-    fi
-
-    ./scripts/feeds update -a && ./scripts/feeds install -a
-
-    ln -s feeds/cgminer/cgminer/root-files files
-    exit 0
-fi
-
-rm -f ./avalon/dl/cgminer-*-avalon*.tar.bz2
-
-if [ "$1" == "--build" ]; then
-    if [ ! -d avalon/openwrt ]; then
-        "$0" --removeall
-        "$0" --clone
-        [ "$?" != "0" ] && echo "[ERROR]: clone failed" && exit 1
-    fi
-    cd avalon/openwrt/
-    LAST_OPENWRT_VER=`cat .openwrt_version`
-    if [ "${LAST_OPENWRT_VER}" != "${OPENWRT_VER}" ]; then
-        echo ${OPENWRT_VER} > .openwrt_version
-        make clean
-    fi
-
-    LAST_AVA_TARGET=`cat .ava_target`
-    if [ "${LAST_AVA_TARGET}" != "${AVA_TARGET_BOARD}" ]; then
-	echo "${AVA_TARGET_BOARD}" > .ava_target
-	make clean
-    fi
-
-    cp ./feeds/cgminer/cgminer/data/${OPENWRT_CONFIG} .config
-    yes "" | make oldconfig
-    make -j${CORE_NUM} V=s IGNORE_ERRORS=m || make V=s IGNORE_ERRORS=m
-    exit $?
-fi
-
-
-## Update all git
-if [ "$1" == "--update" ]; then
-    (cd avalon/cgminer && git pull)
-    (cd avalon/luci    && git pull)
-    (cd avalon/cgminer-openwrt-packages && git pull)
-    cd avalon/openwrt
-    ./scripts/feeds update cgminer; ./scripts/feeds install -a -p cgminer
-    ./scripts/feeds update luci
-    cp ./feeds/cgminer/cgminer/data/${OPENWRT_CONFIG} .config
-    yes "" | make oldconfig
-    exit $?
-fi
-
-
-## Rebuild cgminer
-cd avalon
-make -j${CORE_NUM} -C ${OPENWRT_PATH} package/cgminer/{clean,compile} V=s || make -C ${OPENWRT_PATH} package/cgminer/{clean,compile} V=s
-RET="$?"
-if [ "${RET}" != "0" ] || [ "$1" == "--cgminer" ]; then
-    if [ "${RET}" == "0" ]; then
-        mkdir -p bin/${AVA_TARGET_BOARD}
-        cp ${OPENWRT_PATH}/bin/${AVA_TARGET_PLATFORM}/packages/cgminer/cgminer*.ipk  bin/${AVA_TARGET_BOARD}
-        if [ "${AVA_TARGET_BOARD}" == "tl-wr703n-v1" ]; then
-            cp ${OPENWRT_PATH}/build_dir/target-mips*uClibc-*/cgminer-*/cgminer bin/${AVA_TARGET_BOARD}/cgminer-${AVA_TARGET_PLATFORM}
-        fi
-
-        if [ "${AVA_TARGET_BOARD}" == "pi-modelb-v2" ]; then
-            cp ${OPENWRT_PATH}/build_dir/target-arm*uClibc-*/cgminer-*/cgminer bin/${AVA_TARGET_BOARD}/cgminer-${AVA_TARGET_PLATFORM}
-        fi
-        exit "$?"
-    fi
-    exit "${RET}"
-fi
-
-
-## Build the Web UI & OpenWrt
-DATE=`date +%Y%m%d`
-## Get all repo commit for Avalon version file /etc/avalon_version
-GIT_VERSION=`cd cgminer && git rev-parse HEAD | cut -c 1-7`
-if [ ! -z "`cd cgminer && git status -s -uno`" ]; then
-    GIT_STATUS="+"
-fi
-
-LUCI_GIT_VERSION=`cd luci && git rev-parse HEAD | cut -c 1-7`
-if [ ! -z "`cd luci && git status -s -uno`" ]; then
-    LUCI_GIT_STATUS="+"
-fi
-
-OW_GIT_VERSION=`cd cgminer-openwrt-packages && git rev-parse HEAD | cut -c 1-7`
-if [ ! -z "`cd cgminer-openwrt-packages && git status -s -uno`" ]; then
-    OW_GIT_STATUS="+"
-fi
-
-if [ "${AVA_MACHINE}" != "avalon4" ]; then
-    rm -rf ${LUCI_PATH}/applications/luci-cgminer/dist                                                              && \
-    ( make -j${CORE_NUM} -C ${LUCI_PATH} || make -C ${LUCI_PATH} )                                                  && \
-    cp -a  ${LUCI_PATH}/applications/luci-cgminer/dist/* ${OPENWRT_PATH}/files/
-else
-    make -j${CORE_NUM} -C ${OPENWRT_PATH} package/luci/{clean,compile} V=s || make -C ${OPENWRT_PATH} package/luci/{clean,compile} V=s
-fi
-echo "Avalon Firmware - $DATE"                                  >  ${OPENWRT_PATH}/files/etc/avalon_version     && \
-echo "   luci: $LUCI_GIT_VERSION$LUCI_GIT_STATUS"               >> ${OPENWRT_PATH}/files/etc/avalon_version     && \
-echo "   cgminer: $GIT_VERSION$GIT_STATUS"                      >> ${OPENWRT_PATH}/files/etc/avalon_version     && \
-echo "   cgminer-packages: $OW_GIT_VERSION$OW_GIT_STATUS"       >> ${OPENWRT_PATH}/files/etc/avalon_version     && \
-echo ""					                        >> ${OPENWRT_PATH}/files/etc/avalon_version     && \
-( make -j${CORE_NUM} -C ${OPENWRT_PATH} V=s IGNORE_ERRORS=m || make -C ${OPENWRT_PATH} V=s IGNORE_ERRORS=m )    && \
-mkdir -p bin/${DATE}/${AVA_TARGET_BOARD}/                                                                       && \
-cp -a ${OPENWRT_PATH}/bin/${AVA_TARGET_PLATFORM}/*  bin/${DATE}/${AVA_TARGET_BOARD}/
-rm -rf ${OPENWRT_PATH}/bin/${AVA_TARGET_PLATFORM}
+for i in "$@"
+do
+    case $i in
+        --version|--help)
+            show_help
+            exit
+            ;;
+        --build)
+            prepare_source && prepare_feeds && prepare_config && prepare_version && build_image && do_release
+            ;;
+        --cgminer)
+            prepare_source && prepare_feeds && prepare_config && prepare_version && build_cgminer
+            ;;
+        --cleanup)
+            cleanup
+            ;;
+        *)
+            show_help
+            exit
+            ;;
+    esac
+done
