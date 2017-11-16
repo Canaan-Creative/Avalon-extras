b12f30f58e7aceaf66d8d847698991143b5e1804
firefoxhome <2683299672@qq.com>
2017-11-16 11:35:02 +0800

    Delete loop_auc.sh

--- scripts/factory/loop_auc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-	cd /home/factory/Avalon-extras/scripts/factory
-	bash -c "$*"
-	echo -e "OK OK OK OK OK OK OK OK    OK                   OK\nOK                   OK    OK                OK\nOK                   OK    OK             OK\nOK                   OK    OK          OK\nOK                   OK    OK       OK\nOK                   OK    OK    OK \nOK                   OK    OK OK\nOK                   OK    OK    OK \nOK                   OK    OK       OK\nOK                   OK    OK          OK\nOK                   OK    OK             OK\nOK                   OK    OK                OK\nOK                   OK    OK                   OK\nOK OK OK OK OK OK OK OK    OK                      OK"
-	sleep 3
-done
