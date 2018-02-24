6a3a6b0f293275bcd2e173dab501bd393ba769da
JianXiongZ <jianxiong@canaan-creative.com>
2018-02-24 15:01:34 +0800

    Display the prompting information when complete burn

--- scripts/factory/burn-mm.sh
+++ scripts/factory/burn-mm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/factory/Avalon-extras/scripts/factory
 make isedir=/home/factory/Xilinx/14.6/ISE_DS reflash MM_PLATFORM=$1
+[ -z "$BAR" ] && BAR="+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n$1 Burn Complete\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e $BAR
